7b53d95a4f74a5da382e52f59a699b30feca100e
Peng <17637473650@163.com>
2023-04-15 18:20:49 +0800

    封装表单新增switch 选项

--- src/components/Form/index.vue
+++ src/components/Form/index.vue
@@ -28,7 +28,16 @@
       isShow,
       slotName,
       options,
-      multiple
+      multiple,
+      tValue,
+      fValue,
+      tText,
+      fText,
+      tIcon,
+      fIcon,
+      tBgColor,
+      fBgColor,
+      isInline
     }, i) in formItemList"
       :key="i"
     >
@@ -60,7 +69,6 @@
           :md="md || 24"
           :lg="lg || 24"
           :xl="xl || 24"
-          v-show="(isShow === undefined ? true : isShow)"
         >
 
           <el-input
@@ -90,7 +98,6 @@
           :md="md || 24"
           :lg="lg || 24"
           :xl="xl || 24"
-          v-show="(isShow === undefined ? true : isShow)"
         >
           <el-select
             :multiple="multiple"
@@ -108,129 +115,48 @@
           </el-select>
         </el-col>
       </el-form-item>
-    </template>
-    <!-- <el-form-item label="Activity name">
-      <el-col
-        :span="12"
-        :xs="24"
-        :sm="12"
-        :md="24"
-        :lg="24"
-        :xl="24"
-      >
-        <el-input v-model="form.name" />
-      </el-col>
-    </el-form-item>
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item> -->
-  </el-form>
 
-  <!-- <el-form
-    :model="form"
-    label-width="120px"
-    label-suffix=""
-  >
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="Activity zone">
-      <el-select
-        v-model="form.region"
-        placeholder="please select your zone"
+      <!-- switch 选择器 -->
+      <el-form-item
+        v-else-if="type === 'switch'"
+        :size="size"
+        :prop="prop"
+        :label="label"
+        :rules="rules"
+        :required="required"
+        v-show="(isShow === undefined ? true : isShow)"
       >
-        <el-option
-          label="Zone one"
-          value="shanghai"
-        />
-        <el-option
-          label="Zone two"
-          value="beijing"
-        />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="Activity time">
-      <el-col :span="11">
-        <el-date-picker
-          v-model="form.date1"
-          type="date"
-          placeholder="Pick a date"
-          style="width: 100%"
-        />
-      </el-col>
-      <el-col
-        :span="2"
-        class="text-center"
-      >
-        <span class="text-gray-500">-</span>
-      </el-col>
-      <el-col :span="11">
-        <el-time-picker
-          v-model="form.date2"
-          placeholder="Pick a time"
-          style="width: 100%"
-        />
-      </el-col>
-    </el-form-item>
-    <el-form-item label="Instant delivery">
-      <el-switch v-model="form.delivery" />
-    </el-form-item>
-    <el-form-item label="Activity type">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox
-          label="Online activities"
-          name="type"
-        />
-        <el-checkbox
-          label="Promotion activities"
-          name="type"
-        />
-        <el-checkbox
-          label="Offline activities"
-          name="type"
-        />
-        <el-checkbox
-          label="Simple brand exposure"
-          name="type"
-        />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="Resources">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="Sponsor" />
-        <el-radio label="Venue" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input
-        v-model="form.desc"
-        type="textarea"
-      />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary">Create</el-button>
-      <el-button>Cancel</el-button>
-    </el-form-item>
-  </el-form> -->
+        <el-col
+          :span="span || 24"
+          :offset="offset || 0"
+          :xs="xs || 24"
+          :sm="sm || 24"
+          :md="md || 24"
+          :lg="lg || 24"
+          :xl="xl || 24"
+        >
+          <el-switch
+            :size="size"
+            :placeholder="placeholder"
+            :active-text="tText"
+            :active-value="tValue"
+            :active-icon="tIcon"
+            :inactive-text="fText"
+            :inactive-value="fValue"
+            :inactive-icon="fIcon"
+            :inline-prompt="(isInline === undefined ? false : isInline)"
+            :style="`--el-switch-on-color: ${tBgColor || '#13ce66'} ; --el-switch-off-color: ${fBgColor || '#ff4949'} `"
+            @change="handleSwitchChange($event, prop, i)"
+            v-model="formData[prop]"
+          />
+        </el-col>
+      </el-form-item>
+    </template>
+  </el-form>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive } from 'vue'
-import { Edit } from '@element-plus/icons-vue'
-const form = reactive({
-	name: '',
-	region: '',
-	date1: '',
-	date2: '',
-	delivery: false,
-	type: [],
-	resource: '',
-	desc: '',
-})
-
 interface PengFormAttribute {
 	formData: Record<string, unknown>
 	formItemList: FormItem[]
@@ -254,6 +180,11 @@ const props = withDefaults(defineProps<PengFormAttribute>(), {
 const formRef = ref<any>(null)
 // 获取Form表单的Ref
 const getRef = () => formRef.value
+
+// switch 切换
+const handleSwitchChange = (newVal: any, prop: string, index: number) => {
+	console.log('switch切换 -----', newVal, prop, index)
+}
 
 // 暴露出 可被父组件调用的变量或方法
 defineExpose({ getRef })
