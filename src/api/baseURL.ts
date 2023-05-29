a70e243a67b2737270c88010dddd6d82e3734264
Peng <17637473650@163.com>
2023-05-29 18:38:09 +0800

    请求网关修改

--- src/api/baseURL.ts
+++ src/api/baseURL.ts
@@ -1,8 +1,8 @@
-export const BASE_URL = 'http://127.0.0.1:3000'
-export const WS_URL = 'ws://127.0.0.1:3000'
+// export const BASE_URL = 'http://127.0.0.1:3000'
+// export const WS_URL = 'ws://127.0.0.1:3000'
 
 // export const BASE_URL = 'http://192.168.31.9:3000'
 // export const BASE_URL = 'ws://192.168.31.9:3000'
 
-// export const BASE_URL = 'http://116.204.120.144:3000'
-// export const WS_URL = 'ws://116.204.120.144:3000'
+export const BASE_URL = 'http://116.204.120.144:3000'
+export const WS_URL = 'ws://116.204.120.144:3000'
